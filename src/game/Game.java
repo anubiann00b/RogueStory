9de193c321cb76562ab0f2750f4fecbac1d3c2a0
Shreyas <skraman1999@gmail.com>
2014-04-22 19:45:02 -0700

    First commit.

---
+++ src/game/Game.java
@@ -0,0 +1,46 @@
+package game;
+
+import game.state.StatePlaying;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Game extends StateBasedGame {
+    
+    public static final int STATE_PLAYING = 1;
+    
+    public static int VIEW_SIZE_X;
+    public static int VIEW_SIZE_Y;
+    
+    public Game() {
+        super("Rogue Story");
+    }
+    
+    public static void main(String[] args) {
+        try {
+            AppGameContainer app = new AppGameContainer(new Game());
+            setupAGC(app);
+        } catch (SlickException e) {
+            throw new RuntimeException("Error initializing game: " + e);
+        }
+    }
+
+    @Override
+    public void initStatesList(GameContainer container) throws SlickException {
+        this.addState(new StatePlaying(STATE_PLAYING));
+    }
+    
+    /** Sets up the window. */
+    private static void setupAGC(AppGameContainer app) throws SlickException {
+        VIEW_SIZE_X = 640;
+        VIEW_SIZE_Y = 480;
+        app.setDisplayMode(VIEW_SIZE_X,VIEW_SIZE_Y,false);
+        app.setShowFPS(true);
+        app.setTargetFrameRate(60);
+        app.setAlwaysRender(true);
+        app.setVSync(true);
+        app.setForceExit(true);
+        app.start();
+    }
+}
